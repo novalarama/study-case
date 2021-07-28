77895a96af5c87b69912189c1fab4fbfb4b78ec3
Noval AKbar Ramadhany <novalakbarramadhany@gmail.com>
2021-07-28 12:44:05 +0700

    Upload Baru

--- study-case2.php
+++ study-case2.php
@@ -4,7 +4,8 @@
  * buat fungsi untuk menghitung bilangan faktorial
  * faktorial(5) -> 5 *4*3*2*1 = 120
  */
-function faktorial($n){
+/*
+ function faktorial($n){
 	$angka = 1;
 	$faktorial = 1;
 	 while($angka <= $n){
@@ -16,5 +17,18 @@ function faktorial($n){
 	  return $faktorial;
 }
 echo faktorial(6);
+*/
+function faktorial($number)
+ {
+     $result = 1;
+     for ($i=$number; $i >= 1 ; $i--) { 
+         $result = $result * $i;
+     }
+    //  return $result;
+    echo "Faktorial of $number is $result";
+ }
+  
+//  echo "Faktorial of 10 is ". faktorial(10);
+faktorial(6);
 
 ?>
